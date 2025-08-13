345dda86b5301816d64c8cb321ba3eafe2da6e8c
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-08-13 20:33:09 -0300

    trasn

--- visionhive/src/main/java/com/visionhive/visionhive/model/Branch.java
+++ visionhive/src/main/java/com/visionhive/visionhive/model/Branch.java
@@ -1,13 +1,11 @@
 package com.visionhive.visionhive.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.List;
 
