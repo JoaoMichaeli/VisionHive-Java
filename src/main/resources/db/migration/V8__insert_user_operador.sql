6eb59ba9d5c588beabbe7623d17c87b8309b2693
JoaoMichaeli <joaovictor_de_bem_@hotmail.com>
2025-09-25 19:38:00 -0300

    add operador

---
+++ src/main/resources/db/migration/V8__insert_user_operador.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (id, username, password)
+VALUES (2, 'operadorCM', '$2a$12$yqipcxXd4bZFTxTqkxw7uOIhvCvnfUSK554cS5k.IOhMYlHRH2quu');
+-- Senha: operador123
+
+INSERT INTO user_roles (user_id, roles)
+VALUES (2, 'ROLE_OPERADOR');
